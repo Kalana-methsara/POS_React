8d3a485c1f3f0f6f7cd134d1da8cd535cbb365b8
Kalana-methsara <kalanamethsara53@gmail.com>
2026-04-14 15:55:12 +0530

    make backend

---
+++ POS_Springboot_Backend/src/main/java/lk/ijse/pos_springboot_backend/PosSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.pos_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosSpringbootBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosSpringbootBackendApplication.class, args);
+    }
+
+}
